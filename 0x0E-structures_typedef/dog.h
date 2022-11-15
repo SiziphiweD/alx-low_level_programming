29cfd73646c1b612a30d223fe7c40cea28c72dc9
Siziphiwe <siziphiwes13@gmail.com>
2022-11-14 16:28:26 -0800

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
  */
 typedef struct dog dog_t;
 
-
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
@@ -16,13 +15,11 @@ typedef struct dog dog_t;
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
  */
-
 struct dog
 {
 	char *name;
 	float age;
-	float age;
-
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
